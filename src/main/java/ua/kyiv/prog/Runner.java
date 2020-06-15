f070d5c5dc4b34b2df2fa1e93171b958acf4c378
yemax <egormaksimchuk@gmail.com>
2020-06-15 19:40:10 +0300

    HomeWork1: Complete task1

--- src/main/java/ua/kyiv/prog/Runner.java
+++ src/main/java/ua/kyiv/prog/Runner.java
@@ -2,6 +2,6 @@ package ua.kyiv.prog;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Prog Kyiv UA");
+        System.out.println("Prog Kyiv UA!!!");
     }
 }
